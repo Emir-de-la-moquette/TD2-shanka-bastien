33898b09cba66fa51a3f64895d6c898f77920745
BastienMonet <monet.bastien01@gmail.com>
2024-02-22 09:07:22 +0100

    ajout attribut class cours

--- src/Cours.java
+++ src/Cours.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cours{
+    private Horaire horaires;
+    private List<Groupe> lesGroupes;
+    private List<User> lesUtilisateur;
+    private List<Matiere> lesMatières;
 
 }

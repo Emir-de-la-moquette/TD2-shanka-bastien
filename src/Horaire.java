330145f9eaa06803aa0e74e4ffd5f48749e31c21
BastienMonet <monet.bastien01@gmail.com>
2024-02-22 09:43:08 +0100

    edit

--- src/Horaire.java
+++ src/Horaire.java
@@ -1,11 +1,16 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Horaire {
+    private LocalDateTime horaireDebut;
+    private LocalDateTime horaireFin;
     private List<Salle> lesSalles;
     private List<Cours> lesCours;
 
-    public Horaire() {
+    public Horaire(LocalDateTime horaireDebut, LocalDateTime horaireFin) {
+        this.horaireDebut = horaireDebut;
+        this.horaireFin = horaireFin;
         this.lesSalles = new ArrayList<>();
         this.lesCours = new ArrayList<>();
     }
